c937ddd925538036448baf26430ff40a6e00328e
Derierik <kevinmsotof@outlook.cl>
2024-06-12 11:12:51 -0400

    Entrega JavaScript - CoderHouse

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,26 @@
+const persona = {
+    firstName: "Kevin",
+    lastName: "Soto",
+    userName: "matidev.js",
+    userPass: "CoderHouse123"
+}
+
+const notes = {
+    noteTitle: "To Do",
+    noteDesc: "Monthly reunion with Specialists teams about every checklist reviewed in the last three months",
+    noteDate: "12-jun-24"
+}
+
+
 const $submit = document.getElementById("submit"),
     $password = document.getElementById("userPass"),
     $username = document.getElementById("userName"),
     $visible = document.getElementById("visible");
+
+const $greetings = document.getElementById("greetings"),
+    $noteTitle = document.getElementById("noteTitle"),
+    $noteDesc = document.getElementById("noteDesc"),
+    $noteDate = document.getElementById("noteDate");
 
     document.addEventListener("change", (e) =>{
         if(e.target === $visible){
@@ -19,7 +38,7 @@ const $submit = document.getElementById("submit"),
         if(e.target === $submit){
             if($password.value !== "" && $username.value !== ""){
                 e.preventDefault();
-                verificarUsuario($username.value, $password.value)
+                verificarUsuario($username.value, $password.value);
                 // window.location.href = "home.html";
             }
             else
@@ -28,20 +47,29 @@ const $submit = document.getElementById("submit"),
         }
     })
 
-    const persona = {
-        firstName: "Kevin",
-        lastName: "Soto",
-        userName: "matidev.js",
-        userPass: "CoderHouse123"
-    }
+    document.addEventListener("load", (e)=>{
+        if(e.target === $home){
 
-    // console.log(persona.userName + ' ' + persona.userPass)
+        }
+    })
+
+
+    // console.log(models.persona.userName + ' ' + models.persona.userPass)
     function verificarUsuario(nombre, pass){
         if (nombre === persona.userName && pass === persona.userPass){
             window.location.href = "./templates/home.html";
         }
         else
         {
-            alert("Las credenciales de acceso no son correctas.")
+            alert("Las credenciales de acceso no son correctas.");
         }
+    }
+
+    function viewNotes(){
+        $greetings.innerHTML = "Bienvenido! " + persona.firstName;
+        $noteTitle.innerHTML = notes.noteTitle;
+        $noteDate.innerHTML = notes.noteDate;
+        $noteDesc.innerHTML = notes.noteDesc;
+
+        // console.log(notes.noteTitle + notes.noteDesc + notes.noteDate)
     }
