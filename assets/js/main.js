2615624d9dca76265fc48f3cc78e706893c6f7f0
Derierik <kevinmsotof@outlook.cl>
2024-06-12 16:53:41 -0400

    Entrega JavaScript - CoderHouse

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,32 @@ const persona = {
     userPass: "CoderHouse123"
 }
 
-const notes = {
-    noteTitle: "To Do",
-    noteDesc: "Monthly reunion with Specialists teams about every checklist reviewed in the last three months",
-    noteDate: "12-jun-24"
-}
+// const notes = [
+//         {
+//             id: 1,
+//             noteTitle: "To Do",
+//             noteDesc: "Monthly reunion with Specialists teams about every checklist reviewed in the last three months",
+//             noteDate: "12-jun-24"
+//         },
+//         {
+//             id: 2,
+//             noteTitle: "Supermarket List",
+//             noteDesc: "Monthly reunion with Specialists teams about every checklist reviewed in the last three months",
+//             noteDate: "12-jun-24"
+//         },
+//         {
+//             id: 3,
+//             noteTitle: "Other Notes",
+//             noteDesc: "Monthly reunion with Specialists teams about every checklist reviewed in the last three months",
+//             noteDate: "12-jun-24"
+//         }
+//     ];
+
+const nota1 = new Notas("To Do", "Monthly reunion with Specialists teams about every checklist reviewed in the last three months","12-jun-24");
+
+const nota2 = new Notas("Supermarket List", "Monthly reunion with Specialists teams about every checklist reviewed in the last three months","12-jan-24");
+
+const nota3 = new Notas("Other Notes", "Monthly reunion with Specialists teams about every checklist reviewed in the last three months","12-dec-23");
 
 
 const $submit = document.getElementById("submit"),
@@ -47,12 +68,6 @@ const $greetings = document.getElementById("greetings"),
         }
     })
 
-    document.addEventListener("load", (e)=>{
-        if(e.target === $home){
-
-        }
-    })
-
 
     // console.log(models.persona.userName + ' ' + models.persona.userPass)
     function verificarUsuario(nombre, pass){
@@ -67,9 +82,25 @@ const $greetings = document.getElementById("greetings"),
 
     function viewNotes(){
         $greetings.innerHTML = "Bienvenido! " + persona.firstName + ' '+ persona.lastName;
-        $noteTitle.innerHTML = notes.noteTitle;
-        $noteDate.innerHTML = notes.noteDate;
-        $noteDesc.innerHTML = notes.noteDesc;
+        $noteTitle.innerHTML = nota1.noteTitle;
+        $noteDate.innerHTML = nota1.noteDate;
+        $noteDesc.innerHTML = nota1.noteDesc;
+
+        document.getElementById('noteTitle1').innerHTML = nota2.noteTitle;
+        document.getElementById('noteDate1').innerHTML = nota2.noteDate;
+        document.getElementById('noteDesc1').innerHTML = nota2.noteDesc;
+
+        document.getElementById('noteTitle2').innerHTML = nota3.noteTitle;
+        document.getElementById('noteDate2').innerHTML = nota3.noteDate;
+        document.getElementById('noteDesc2').innerHTML = nota3.noteDesc;
+        
 
         // console.log(notes.noteTitle + notes.noteDesc + notes.noteDate)
+    }
+
+    function Notas(title, desc, date){
+        this.noteTitle = title;
+        this.noteDesc = desc;
+        this.noteDate = date;
+
     }
