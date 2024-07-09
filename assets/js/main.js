0461eedb9be5a0da862848e13a0da6885fa8e182
Derierik <kevinmsotof@outlook.cl>
2024-07-09 15:31:49 -0400

    JavaScript - CoderHouse

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,66 @@
-const $tipo = document.getElementById("tipo"),
-    $plazo = document.getElementById("plazo"),
-    $haberes = document.getElementById("haberes"),
-    $base = document.getElementById("base"),
-    $ausente = document.getElementById("ausente"),
-    $cant_ausente = document.getElementById("cant-ausente"),
-    $gratificacion = document.getElementById("gratificacion"),
-    $valor_grat = document.getElementById("valor_grat"),
-    $hhee = document.getElementById("hhee"),
-    $valor_hhee = document.getElementById("valor-hhee"),
-    $otrosa = document.getElementById("otrosa"),
-    $otrosb = document.getElementById("otrosb"),
-    $otrosc = document.getElementById("otrosc"),
-    $colacion = document.getElementById("colacion"),
-    $movilizacion = document.getElementById("movilizacion"),
-    $asignacion = document.getElementById("asignacion")
-    ;
-
-const $afp = document.getElementById("afp"),
-    $valor_afp = document.getElementById("valor-afp"),
-    $fonasa = document.getElementById("fonasa"),
-    $valor_fonasa = document.getElementById("valor-fonasa"),
-    $seguro = dcument.getElementById("seguro"),
-    $impuesto = document.getElementById("impuesto"),
-    $apv = document.getElementById("apv"),
-    $otros_dsctos_legales = document.getElementById("otros-dsctos-legales"),
-    $otros_dsctos = document.getElementById("otros-dsctos")
-    ;
-
-
-function calcularLiquido(){
-
-    const form = document.createElement('form');
-    form.action ='';
-    form.method = 'GET';
     
-    const $per_gratificacion = 0.25;
-
-    const $valorgrat = $base.value * $per_gratificacion;
-
-    $valor_grat.innerHTML = $valorgrat.toString();
-
-    alert("se ejecuto la funcion")
+    let base = document.getElementById("base"),
+            grat = document.getElementById("gratificacion"),
+            valor_grat = document.getElementById("valor_grat"),
+            ausente = document.getElementById("ausente")
 
     
-} 
+    
+    
+    btnCalcular.addEventListener('click', () => {
+        calcular();
+    })
+
+    base.addEventListener('onkeydown', () => {
+        calcular();
+    })
+
+    ausente.onchange = function() {
+        alert("esta wea cambio")
+    }
+    
+    // calcular.addEventListener('click', ()=>{
+        //     calcular();
+        // })
+        
+    function calcular(){
+
+        if(base.value !== ""){
+
+            console.log(gratificacion.value)
+            if (gratificacion.value == 0){
+                let valor_imm = 500.000
+                let porcentaje_grat = 0.25
+                let valor_por_imm = 4.75
+                porcentaje_grat *= base.value
+                valor_por_imm *= valor_imm / 12
+    
+                if(porcentaje_grat < valor_por_imm){
+                    document.getElementById("valor_grat").value = porcentaje_grat
+    
+                }
+                else
+                {
+                    document.getElementById("valor_grat").value = valor_por_imm
+                }
+    
+                
+    
+                console.log(porcentaje_grat.toString())
+
+            }
+            else
+            {
+                valor_grat.value = ""
+            }
+
+        }
+        else
+        {
+            alert('Debe ingresar un valor en -Sueldo Base-')
+        }
+        // alert('se ejecuto esta wea')
+    }
 
 
 
