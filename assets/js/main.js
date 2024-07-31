bb9c10b63ff44122ed45d9d1156135c8a0f0a200
unknown <kevinmsotof@outlook.cl>
2024-07-30 21:16:52 -0400

    Calculadora con monto de Horas Extras Listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,9 @@
 
     fonasa.onchange = function() {
 
+        // se aplica el evento 'onchange' ya que es necesario habilitar el campo valor_fonasa para cuando el usuario seleccione la opcion "Isapre", ya que se necesita ingresar el valor del cobro por UF (Unidad de Fomento, Chile)
+
+
         if(fonasa.value == 0){
             valor_fonasa.value = ""
             valor_fonasa.disabled = true;
@@ -34,32 +37,45 @@
         }
 
     }
-
-    const users = {
-        user : "coderhouse",
-        pass : "CoderHouse123"
-
-    }
-    
-    console.log(users.user)
-    // calcular.addEventListener('click', ()=>{
-        //     calcular();
-        // })
         
     function calcular(){
 
         if(base.value !== ""){
 
             if (gratificacion.value == 0){
-                let valor_imm = 500.000
-                let porcentaje_grat = 0.25
-                let valor_por_imm = 4.75
+
+                //Se inicializan variables con valor de ingreso minimo mensual y porcentaje del 25% del sueldo base
+
+                let valor_imm = 500000
+                let porcentaje_grat = 25
+
+                //calcular gratificacion por el ingreso minimo mensual
+
+                let valor_por_imm = 475
                 porcentaje_grat *= base.value
                 valor_por_imm *= valor_imm / 12
+
+                // Se agregara el valor de gratificacion que sea menor, en caso de que el sueldo base sea de 1,000,000 CLP el valor de gratificacion del 25% sera de 250
+                // El valor de la gratificacion calculado con el ingreso minimo mensual sera de 197,167 CLP aprox. esto quiere decir que se sumara como gratificacion este valor, ya que es menor que 250,000 CLP
+
+
+
+                //Calculo de valor de Horas Extra
+
+
 
                 let valor_horas, valor_hhee, valor_total_hhee = 0;
 
                 if (document.getElementById("hhee").value !== ""){
+
+
+                    // Calculo de valor de horas extras
+                    // Se divide el sueldo bruto por la cantidad de dias en el mes o dias trabajados (30)
+                    //luego se multiplica por 28 y se divide por 176
+                    //al resultado se le multiplica por 1,5 que es el valor agregado de las horas extras
+
+
+
                     valor_horas = base.value / 30;
                     valor_hhee = valor_horas * 28;
                     valor_hhee = valor_hhee / 176;
@@ -69,54 +85,74 @@
                 }
     
                 if(porcentaje_grat < valor_por_imm){
-                    document.getElementById("valor_grat").value = porcentaje_grat.toFixed(3)
+                    document.getElementById("valor_grat").value = parseInt(porcentaje_grat)
     
                 }
                 else
                 {
-                    document.getElementById("valor_grat").value = valor_por_imm.toFixed(3)
-                }
+                    document.getElementById("valor_grat").value = parseInt(valor_por_imm)
+
+
+
+                    //Calculo descuentos legales (AFP y Fonasa)
+                    //Descuento para AFP : porcentaje de interes mas el 10% del sueldo bruto
+                    //Descuento Para Fonasa : Porcentaje del 7% del sueldo bruto
+
+
+
+                    if(afp.value !== 0){
+
+                        console.log(afp.value)
     
-                if(afp.value !== 0){
-
-                    console.log(afp.value)
-
-                    let comm_afp = 0;
-
-                    switch(afp.value){
-                        case 1: //  C A P I T A L
-                            comm_afp = 0.0144
-                            
-                            break;  
-                        case 2: //  C U P R U M
-                            comm_afp = 0.0144
-                            break;
-                        case 3: //  H A B I T A T
-                            comm_afp = 0.0127
-                            break;
-                        case 4: //  M O D E L O
-                            comm_afp = 0.0058
-                            break;
-                        case 5: // P L A N V I T A L
-                            comm_afp = 0.0116
-                            break;
-                        case 6: // P R O V I D A
-                            comm_afp = 0.0145
-                            break;
-                        case 7: // U N O 
-                            comm_afp = 0.0049
-                            break;
-                        case 8: // P R I M E R  T R A B A J O
-                            break;
-                        default:    // N O  C O T I Z A
-                            break;
-                        }    
-
-                        // comm_afp = comm_afp + 0.1;
-
-                        valor_afp.value = comm_afp.toFixed(4);
-                        console.log(comm_afp)
+                        var comm_afp,sueldobruto = 0;
+    
+                        switch(afp.value){
+                            case "1": //  C A P I T A L
+                                console.log("Capital")
+                                comm_afp = 0.0144
+                                
+                                break;  
+                            case "2": //  C U P R U M
+                                console.log("Cuprum")
+                                comm_afp = 0.0144
+                                break;
+                            case "3": //  H A B I T A T
+                                console.log("Habitat")  
+                                comm_afp = 0.0127
+                                break;
+                            case "4": //  M O D E L O
+                                console.log("Modelo")
+                                comm_afp = 0.0058
+                                break;
+                            case "5": // P L A N V I T A L
+                            console.log("Planvital")
+                                comm_afp = 0.0116
+                                break;
+                            case "6": // P R O V I D A
+                            console.log("Provida")
+                                comm_afp = 0.0145
+                                break;
+                            case "7": // U N O 
+                            console.log("Uno")
+                                comm_afp = 0.0049
+                                break;
+                            case "8": // P R I M E R  T R A B A J O
+                                break;
+                            default:    // N O  C O T I Z A
+                                break;
+                            }    
+    
+                            comm_afp = comm_afp + 0.1;
+                            sueldobruto = base.value * 1
+                            sueldobruto = sueldobruto + (valor_grat.value * 1)
+                            console.log(sueldobruto)
+                            valor_afp.value = comm_afp.toFixed(4);
+                            console.log(comm_afp)
+                    }
                 }
+
+    
+                
             }
             else
             {
